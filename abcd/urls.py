d05989901c746c50c0fbe217ddd36a2a1548b1e7
gabriel-1607 <gabrielambrosini02@gmail.com>
2025-07-03 15:06:09 -0500

    Prueba de funcionalidad con mensaje hola mundo

--- abcd/urls.py
+++ abcd/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from acerca import views
 
 urlpatterns = [
+    path('', views.indice, name='indice'),
     path('admin/', admin.site.urls),
 ]
